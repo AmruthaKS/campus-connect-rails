6fa02c7f6e4cbeb7893954c48a0e5d27eb15a3a1
Shiv <shiva.n404@gmail.com>
2013-06-24 08:25:23 +0530

    Revert "Revert "-Just pushing code from local""
    This reverts commit 49543cc8d14dbd5630720d451359c9c7b2d18bfc.

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,11 @@
 module NotificationsHelper
+
+  def get_string_to_be_printed(notification)
+    if notification.tContent_type.eql? USER_TCONTENT_TYPE
+
+    elsif notification.tContent_type.eql? COLLEGE_TCONTENT_TYPE
+
+    end
+
+  end
 end
