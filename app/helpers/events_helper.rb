2fe0c3325ece6e73a605086ad09514ee196f9cd8
Shiv <shiva.n404@gmail.com>
2013-06-25 22:39:41 +0530

    -Adding notifications with target links code

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,18 @@
 module EventsHelper
+  def get_tcontent_string_for_event(event)
+    if event.tContent_type.eql? USER_TCONTENT_TYPE
+
+    elsif event.tContent_type.eql? COLLEGE_TCONTENT_TYPE
+      @college = College.find(event.tContent_id)
+      @college.name
+    end
+  end
+
+  def get_tcontent_object_for_event(event)
+    if event.tContent_type.eql? USER_TCONTENT_TYPE
+
+    elsif event.tContent_type.eql? COLLEGE_TCONTENT_TYPE
+      @college = College.find(event.tContent_id)
+    end
+  end
 end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,11 +1,15 @@
 module NotificationsHelper
 
-  def get_string_to_be_printed(notification)
+  def get_string_to_be_printed_for_notification(notification)
     if notification.tContent_type.eql? USER_TCONTENT_TYPE
 
     elsif notification.tContent_type.eql? COLLEGE_TCONTENT_TYPE
-
+      College.find(notification.tContent_id)
     end
+
+  end
+
+  def get_object_to_be_printed_for_notification
 
   end
 end
